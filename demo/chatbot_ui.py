7b3d6ac011b81640f7efab680d11176b7fd438d2
choidaedae <daehyeonchoi@postech.ac.kr>
2024-11-15 14:30:51 +0900

    init

---
+++ demo/chatbot_ui.py
@@ -0,0 +1,52 @@
+import openai
+from getpass import getpass
+from typing import Optional
+import gradio as gr
+
+openai.api_key = getpass('Enter your OpenAI API Key: ')
+
+class Chat:
+
+    def __init__(self, system: Optional[str] = None):
+        self.system = system
+        self.messages = []
+        
+        if system is not None:
+            self.messages.append({
+                "role": "system",
+                "content": system
+            })
+
+    def prompt(self, content: str) -> str:
+          self.messages.append({
+              "role": "user",
+              "content": content
+          })
+          response = openai.ChatCompletion.create(
+              model="gpt-3.5-turbo",
+              messages=self.messages
+          )
+          response_content = response["choices"][0]["message"]["content"]
+          self.messages.append({
+              "role": "assistant",
+              "content": response_content
+          })
+          return response_content
+      
+chat = Chat(system="You are a helpful assistant.")
+
+def respond(message, chat_history):
+    bot_message = chat.prompt(content=message)
+    chat_history.append((message, bot_message))
+    return "", chat_history
+
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    clear.click(lambda: None, None, chatbot, queue=False)
+
+demo.launch(debug=True)
